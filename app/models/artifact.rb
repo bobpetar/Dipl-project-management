d3be5cd788c9f163a8eff62bd87783cbfab1f05d
bobpetar <bobpetar@gmail.com>
2016-10-10 16:37:50 +0000

    complete artifacts

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -5,6 +5,7 @@ class Artifact < ActiveRecord::Base
   validates_presence_of :name, :upload
   validates_uniqueness_of :name
   validate :uploaded_file_size
+  before_save :upload_to_s3
   
   private
   
@@ -12,6 +13,14 @@ class Artifact < ActiveRecord::Base
     if upload
      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
     end
+  end
+  
+  def upload_to_s3
+    s3 = Aws::S3::Resource.new
+    tenant_name = Tenant.find(Thread.current[:tenant_id]).name
+    obj = s3.bucket(ENV['S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+    obj.upload_file(upload.path, acl:'public-read')
+    self.key = obj.public_url
   end
   
 end
